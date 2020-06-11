517474983affe87cf24bf29dd4095ce70d7ecc27
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-11 00:59:47 -0300

    components: create component for pokemon info

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
     <div id="app">
         <div v-if="!isLoading">
             <p>pokedex</p>
+
             <p>pokemon counter: {{pokemonCount}}</p>
             <div class="cards">
                 <div v-for="(p, index) in pokemon" :key="index">
-                    <ul>
-                        <li>{{p.name}}</li>
-                    </ul>
+                    <PokemonCard :pokemon="p" :pokemonId="index" />
                 </div>
             </div>
         </div>
@@ -18,8 +17,13 @@
 import { mapGetters } from "vuex";
 import { FETCH_POKEMON } from "@/store/type/actions";
 
+import PokemonCard from "@/components/PokemonCard";
+
 export default {
     name: "App",
+    components: {
+        PokemonCard
+    },
     computed: {
         ...mapGetters(["pokemonCount", "isLoading", "pokemon"])
     },
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="cards">
+        <div class="card">
+            <div class="image">
+                <img
+                    crossorigin="anonymous"
+                    ref="picture"
+                    width="100"
+                    :src="`https://pokeres.bastionbot.org/images/pokemon/${pokemonId + 1}.png`"
+                />
+                <br />
+                <br />
+                {{ pokemon.name }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PokemonCard",
+    props: ["pokemon", "pokemonId"]
+};
+</script>
+
+<style scoped>
+.image {
+    color: #e1ede7;
+    border: 1px solid #eee;
+    border-radius: 10px;
+    padding: 10px;
+    margin: 5px 0;
+    position: relative;
+    overflow: hidden;
+}
+
+.image img {
+    /* position: absolute; */
+    max-width: 500px;
+    width: 200px;
+    max-height: 500px;
+    height: 200px;
+}
+
+img {
+    display: block;
+    border: 0;
+    width: 90%;
+    height: auto;
+}
+
+.card {
+    background: white;
+    margin-bottom: 2em;
+}
+
+.card a {
+    color: black;
+    text-decoration: none;
+}
+
+.card a:hover {
+    box-shadow: 3px 3px 8px hsl(0, 0%, 80%);
+}
+
+.card-content {
+    padding: 1.4em;
+}
+
+.card-content h2 {
+    margin-top: 0;
+    margin-bottom: 0.5em;
+    font-weight: bold;
+}
+
+.card-content p {
+    font-size: 80%;
+}
+
+.cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.card {
+    flex: 0 1 calc(20% - 1em);
+}
+</style>
