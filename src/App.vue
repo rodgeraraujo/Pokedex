0f1ea0d7167fcd81cae908c46981e148adea0a52
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-11 00:41:20 -0300

    feat: listing pokemon data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
     <div id="app">
-        <p>pokedex</p>
+        <div v-if="!isLoading">
+            <p>pokedex</p>
+            <p>pokemon counter: {{pokemonCount}}</p>
+            <div class="cards">
+                <div v-for="(p, index) in pokemon" :key="index">
+                    <ul>
+                        <li>{{p.name}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { FETCH_POKEMON } from "@/store/type/actions";
+
 export default {
-    name: "App"
+    name: "App",
+    computed: {
+        ...mapGetters(["pokemonCount", "isLoading", "pokemon"])
+    },
+    mounted() {
+        this.fetchPokemon();
+    },
+    methods: {
+        fetchPokemon() {
+            this.$store.dispatch(FETCH_POKEMON);
+        }
+    }
 };
 </script>
 
