35fb98a38cce74cb2cccd202d52fc85b6ebcc2a3
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-14 13:36:45 -0300

    style: re-design nav (version 3)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,39 +1,53 @@
 <template>
-    <nav class="navbar">
-        <span class="navbar-toggle" id="js-navbar-toggle">
-            <div class="nav-btn">
-                <label for="nav-check">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </label>
+    <div>
+        <header>
+            <a class="logo" href="/">
+                <img src="@img/pokedex.svg" alt="logo" />
+            </a>
+            <nav>
+                <ul class="nav-links">
+                    <li>
+                        <router-link to="/">Home</router-link>
+                    </li>
+
+                    <li>
+                        <router-link to="/generations">Generations</router-link>
+                    </li>
+                </ul>
+            </nav>
+            <router-link class="cta" to="/github">Github</router-link>
+            <p class="menu cta">Menu</p>
+        </header>
+        <div class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay-content">
+                <router-link to="/">Home</router-link>
+                <router-link to="/generations">Generations</router-link>
+                <router-link to="/github">Github</router-link>
             </div>
-        </span>
-        <a href="#" class="logo">Pokedex!</a>
-        <ul class="main-nav" id="js-menu">
-            <li>
-                <router-link class="nav-links" to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link class="nav-links" to="/github">Github</router-link>
-            </li>
-        </ul>
-    </nav>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: "NavBar",
     mounted() {
-        this.toggleMeny();
+        this.toggleMenu();
     },
     methods: {
-        toggleMeny() {
-            let mainNav = document.getElementById("js-menu");
-            let navBarToggle = document.getElementById("js-navbar-toggle");
+        toggleMenu() {
+            const doc = document;
+            const menuOpen = doc.querySelector(".menu");
+            const menuClose = doc.querySelector(".close");
+            const overlay = doc.querySelector(".overlay");
 
-            navBarToggle.addEventListener("click", function() {
-                mainNav.classList.toggle("active");
+            menuOpen.addEventListener("click", () => {
+                overlay.classList.add("overlay--active");
+            });
+
+            menuClose.addEventListener("click", () => {
+                overlay.classList.remove("overlay--active");
             });
         }
     }
@@ -41,115 +55,129 @@ export default {
 </script>
 
 <style scoped>
-* {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 10%;
+    border-radius: 5px;
+    background-color: #444868;
 }
 
-body {
-    font-family: "Josefin Sans", sans-serif;
+.logo {
+    cursor: pointer;
 }
 
-.navbar {
-    font-size: 18px;
-    background: #4a4d68;
-    border-radius: 10px;
-    border: 1px solid #3a3f68;
-    padding-bottom: 10px;
+.nav-links a,
+.cta,
+.overlay-content a {
+    font-weight: bold;
+    font-weight: 500;
+    color: #edf0f1;
+    text-decoration: none;
 }
 
-.main-nav {
-    list-style-type: none;
+.nav-links {
+    list-style: none;
+    display: flex;
+}
+
+.nav-links li {
+    padding: 0px 20px;
+}
+
+.nav-links li a {
+    transition: color 0.3s ease 0s;
+}
+
+.nav-links li a:hover {
+    color: #7c84be;
+}
+
+.cta {
+    padding: 9px 25px;
+    background-color: rgba(0, 136, 169, 1);
+    border: none;
+    border-radius: 50px;
+    cursor: pointer;
+    transition: background-color 0.3s ease 0s;
+}
+
+.cta:hover {
+    background-color: rgba(0, 136, 169, 0.8);
+}
+
+/* mobile Nav */
+
+.menu {
     display: none;
 }
 
-.nav-links,
-.logo {
-    text-decoration: none;
-    color: rgba(255, 255, 255, 0.7);
+.overlay {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    background-color: #24252a;
+    /* background-image: url(../assets/images/poke-bg.png);
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: 0 -10%; */
+    overflow-x: hidden;
+    transition: width 0.5s ease 0s;
 }
 
-.main-nav li {
-    text-align: center;
-    margin: 15px auto;
+.overlay--active {
+    width: 100%;
 }
 
-.logo {
-    display: inline-block;
-    font-size: 22px;
-    margin-top: 10px;
-    margin-left: 20px;
+.overlay-content {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
-.navbar-toggle {
-    position: absolute;
-    top: 10px;
-    right: 20px;
-    cursor: pointer;
-    color: rgba(255, 255, 255, 0.8);
-    font-size: 24px;
-}
-
-.active {
+.overlay a {
+    padding: 15px;
+    font-size: 36px;
     display: block;
+    transition: color 0.3s ease 0s;
 }
 
-.navbar-toggle > .nav-btn {
-    display: inline-block;
+.overlay a:hover,
+.overlay a:focus {
+    color: #4f5479;
+}
+.overlay .close {
     position: absolute;
-    right: 0px;
-    top: 16px;
-}
-.navbar-toggle > .nav-btn > label {
-    display: inline-block;
-    width: 50px;
-    height: 50px;
-    padding: 13px;
+    top: 20px;
+    right: 45px;
+    font-size: 60px;
+    color: #edf0f1;
 }
 
-.navbar-toggle > .nav-btn > label > span {
-    display: block;
-    width: 25px;
-    height: 10px;
-    border-top: 2px solid #eee;
+@media screen and (max-height: 450px) {
+    .overlay a {
+        font-size: 20px;
+    }
+    .overlay .close {
+        font-size: 40px;
+        top: 15px;
+        right: 35px;
+    }
 }
 
-@media screen and (min-width: 630px) {
-    .navbar {
-        display: flex;
-        justify-content: space-between;
-        padding-bottom: 0;
-        height: 70px;
-        align-items: center;
-    }
-
-    .main-nav {
-        display: flex;
-        margin-right: 30px;
-        flex-direction: row;
-        justify-content: flex-end;
-    }
-
-    .main-nav li {
-        margin: 0;
-    }
-
-    .nav-links {
-        margin-left: 40px;
-    }
-
-    .logo {
-        margin-top: 0;
-    }
-
-    .navbar-toggle {
+@media only screen and (max-width: 800px) {
+    .nav-links,
+    .cta {
         display: none;
     }
-
-    .logo:hover,
-    .nav-links:hover {
-        color: rgba(255, 255, 255, 1);
+    .menu {
+        display: initial;
     }
 }
 </style>
